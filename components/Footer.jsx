2fa3c07423b58c704971a370b37dac49f6cb0317
Kaushik Barua <kbarua21@gmail.com>
2022-09-21 21:30:40 +0530

    first commit

---
+++ components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Footer = () => {
+  return (
+    <div className='footer-container'>
+        <p>
+          2022 Kaushik Barua. All rights reserved.
+        </p>
+    </div>
+  )
+}
+
+export default Footer
---
+++ pages/success.js
@@ -0,0 +1,39 @@
+import React,{useEffect} from 'react'
+import Link from 'next/link';
+import {BsBagCheckFill} from 'react-icons/bs';
+import { runFireworks } from '../lib/utils';
+
+import { useStateContext } from '../context/StateContext';
+
+const Success = () => {
+
+  const {setCartItems,setTotalPrice,settotalQuantities,setShowCart}=useStateContext();
+
+  useEffect(()=>{
+    localStorage.clear();
+    setCartItems([]);
+    setTotalPrice(0);
+    settotalQuantities(0);
+    runFireworks();
+    setShowCart(false);
+  },[])
+  return (
+    <div className='success-wrapper'>
+      <div className='success'>
+        <p className='icon'>
+          <BsBagCheckFill />          
+        </p>
+        <h2>Thank you for your order!</h2>
+        <p className='email-msg'>Your food is being prepared. Until then enjoy the ambience</p>
+        <p className='description'>
+          If you want to tell us something. Feel free to call us
+        </p>
+        <Link href='/'>
+          <button type='button' width='300px' className='btn'>Continue browsing!</button>
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default Success
